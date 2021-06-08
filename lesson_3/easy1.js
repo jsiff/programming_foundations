268d504fdb4a3f3493c13718b1c2c03ba2cb21ea
Julian Sifford <julsiff@gmail.com>
2021-06-07 21:23:36 -0400

    adding problems

---
+++ lesson_3/easy1.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3];
+numbers[6] = 5; 
+console.log(numbers);
+
+let str1 = "Come over here!"
+console.log(str1.endsWith("!"));
+
+let str2 = "What's up Doc?"
+console.log(str2.endsWith("!"));
