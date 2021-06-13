8c4fc7bfe5666bb66779142f771533010f3468f6
Julian Sifford <julsiff@gmail.com>
2021-06-13 01:09:31 -0400

    updating

--- lesson_3/easy3.js
+++ lesson_3/easy3.js
@@ -1,4 +1,4 @@
-let numbers = [1, 2, 3, 4];
+// let numbers = [1, 2, 3, 4];
 
 // let i = 0;
 
@@ -27,7 +27,44 @@ let numbers = [1, 2, 3, 4];
 // numbers.splice(0, numbers.length);
 // console.log(numbers);
 
-while (numbers.length) {
-  numbers.pop();
-}
-console.log(numbers);
+// while (numbers.length) {
+//   numbers.pop();
+// }
+// console.log(numbers);
+
+// Question 2
+
+// console.log([1, 2, 3] + [4, 5]); // converts array to string object 34 
+
+// Question 3
+
+let str1 = "hello there";
+let str2 = str1;
+str2 = "goodbye!";
+console.log(str1); // "hello there"
+
+// Question 4
+
+let arr1 = [{ first: "value1" }, { second: "value2" }, 3, 4, 5];
+let arr2 = arr1.slice();
+arr2[0].first = 42;
+console.log(arr1);
+console.log(arr2);
+
+// Question 5
+
+// function isColorValid(color) {
+//   if (color === "blue" || color === "green") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function isColorValid(color) {
+//   return color === "blue" || color === "green";
+// }
+
+// const isColorValid = color => color === "blue" || color === "green";
+
+// const isColorValid = color => ["blue", "green"].includes(color);
