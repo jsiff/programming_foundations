b5dcac3c4df7a0399ba4547992f7160556f86d7e
Julian Sifford <julsiff@gmail.com>
2021-06-15 22:29:15 -0400

    updating

--- lesson_3/medium1.js
+++ lesson_3/medium1.js
@@ -23,11 +23,29 @@ let munsterDescription = "The Munsters are creepy and spooky.";
 
 // console.log(alternateCase(munsterDescription));
 
-console.log(munsterDescription.split("").map(function(char) {
-  if (char === char.toUpperCase()) {
-    return char.toLowerCase();
-  } else {
-    return char.toUpperCase();
-  }
-}).join(""));
+// console.log(munsterDescription.split("").map(function(char) {
+//   if (char === char.toUpperCase()) {
+//     return char.toLowerCase();
+//   } else {
+//     return char.toUpperCase();
+//   }
+// }).join(""));
 
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  // needs to catch 0 and negative values before loop
+  if (number === 0 || number.toString().match(/^-\d+$/)) {
+    console.log("Please enter non-zero and non-negative numbers only.");
+    return;
+  } else {
+  do {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  } while (divisor != 0);
+  return factors;
+  }
+}
+console.log(factors(-20));
