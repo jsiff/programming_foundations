b93eb91de9a35c42cd4a684d7581ef456ae5ec34
Julian Sifford <julsiff@gmail.com>
2021-06-16 01:44:48 -0400

    updating

--- lesson_3/medium1.js
+++ lesson_3/medium1.js
@@ -31,21 +31,56 @@ let munsterDescription = "The Munsters are creepy and spooky.";
 //   }
 // }).join(""));
 
-function factors(number) {
-  let divisor = number;
-  let factors = [];
-  // needs to catch 0 and negative values before loop
-  if (number === 0 || number.toString().match(/^-\d+$/)) {
-    console.log("Please enter non-zero and non-negative numbers only.");
-    return;
-  } else {
-  do {
-    if (number % divisor === 0) {
-      factors.push(number / divisor);
-    }
-    divisor -= 1;
-  } while (divisor != 0);
-  return factors;
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   // needs to catch 0 and negative values before loop
+//   if (number === 0 || number.toString().match(/^-\d+$/)) {
+//     console.log("Please enter non-zero and non-negative numbers only.");
+//     return;
+//   } else {
+//   do {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor -= 1;
+//   } while (divisor != 0);
+//   return factors;
+//   }
+// }
+
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   while (divisor > 0) {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor -= 1;
+//   } while (divisor !== 0);
+//   return factors;
+// }
+// console.log(factors(0));
+
+// Question 4
+// answer concat does not mutate the original array but push does
+
+function addToRollingBuffer1(buffer, maxBufferSize, newElement) {
+  buffer.push(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
   }
+  return buffer;
 }
-console.log(factors(-20));
+
+function addToRollingBuffer2(buffer, maxBufferSize, newElement) {
+  buffer = buffer.concat(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+
+// Question 5
+// answer console.log(0.3 + 0.6);  returns floating point number
+// answer console.log(0.3 + 0.6 === 0.9); returns false 
