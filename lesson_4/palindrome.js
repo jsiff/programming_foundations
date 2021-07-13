c6520cfc8c0159c64f4fcd90d5839e9e88d4b540
Julian Sifford <julsiff@gmail.com>
2021-07-12 22:48:21 -0400

    code practice

---
+++ lesson_4/palindrome.js
@@ -0,0 +1,39 @@
+function substrings(str) {
+  let result = [];
+  let startingIndex = 0;
+
+  while (startingIndex <= str.length - 2) {
+    let numChars = 2;
+    while (numChars <= str.length - startingIndex) {
+      let substring = str.slice(startingIndex, startingIndex + numChars);
+      result.push(substring);
+      numChars += 1;
+    }
+
+    startingIndex += 1;
+  }
+
+  return result;
+}
+
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
+
+function palindromeSubstrings(str) {
+  let result = [];
+  let substringsArr = substrings(str);
+
+  substringsArr.forEach(substring => {
+    if (isPalindrome(substring)) {
+      result.push(substring);
+    }
+  });
+
+  return result;
+}
+
+console.log(palindromeSubstrings("supercalifragilisticexpialidocious")); // ["ili"]
+console.log(palindromeSubstrings("abcddcbA"));   // ["bcddcb", "cddc", "dd"]
+console.log(palindromeSubstrings("palindrome")); // []
+console.log(palindromeSubstrings(""));           // []
